d36f64b1c0201072f620808f7780ac8ee6f20a23
unknown <berkeleyplatte989@gmail.com>
2020-04-28 21:37:31 -0500

    initial commit

---
+++ pusher.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import subprocess
+from imports import insert
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+with open('the_pusher.txt', 'w') as update:
+    update.write(f'{today}')
+
+subprocess.Popen(['C:/Program Files/Git/bin/git', 'add', '.'])
+subprocess.Popen(['C:/Program Files/Git/bin/git', 'commit', '-m', 'minor changes'])
