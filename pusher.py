75d91fce2192e1448f86467a1966cfd417b91fc7
unknown <berkeleyplatte989@gmail.com>
2020-04-28 21:48:58 -0500

    "minor changes"

--- pusher.py
+++ pusher.py
@@ -9,4 +9,4 @@ with open('text.txt', 'w') as update:
 
 subprocess.Popen(['C:/Program Files/Git/bin/git', 'add', '.'])
 subprocess.Popen(['C:/Program Files/Git/bin/git', 'commit', '-m', '"minor changes"'])
-subprocess.Popen(['C:/Program Files/Git/bin/git', '^C'])
+subprocess.end()
